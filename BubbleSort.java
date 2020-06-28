d08acfc9bc55e4ef40b8875dbbbcd4a3950fb0ff
史雨鑫 <dalianmao22233@shius-MacBook-Pro.local>
2020-06-27 17:49:28 -0700

    add first file

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,7 +6,6 @@ public class BubbleSort {
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
                 if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[i]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -20,11 +19,10 @@ public class BubbleSort {
         System.out.println();
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         BubbleSort ob = new BubbleSort();
         int arr[] = {64, 34, 25, 12, 22, 11, 90};
         ob.bubbleSort(arr);
-        System.out.println("Sorted array");
         ob.printArray(arr);
     }
 }
