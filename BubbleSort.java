ac0e222048302788fd68636affe093e1a1e47b1f
史雨鑫 <dalianmao22233@shius-MacBook-Pro.local>
2020-06-27 17:49:28 -0700

    add first file

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,21 +3,20 @@ import java.util.*;
 public class BubbleSort {
     public void bubbleSort(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
-                {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
                     // swap arr[j+1] and arr[i]
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
     }
 
     /* Prints the array */
     public void printArray(int arr[]) {
         int n = arr.length;
-        for (int i=0; i<n; ++i)
+        for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
